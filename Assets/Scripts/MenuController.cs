6078eeda5b157bba246814fae68b181c97799583
STiCh <tchen@edu.tecnocampus.cat>
2025-01-26 20:03:31 +0100

    Ending Scene
    Show ending message.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,27 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+    float end = 0f;
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(end);
         if (Input.GetKeyDown("escape"))
         {
             QuitGame();
+            Debug.Log("salir");
         }
         if (Input.GetKeyDown("return"))
         {
             LoadGame();
+            Debug.Log("cargar");
+            end = 0f;
+        }
+        if(player.transform.position.y < -4f && end == 0f)
+        {
+            EndGame();
+            Debug.Log("fin");
+            end = 1f;
         }
     }
 
     public void LoadGame()
     {
+        Time.timeScale = 1.0f;
         SceneManager.LoadScene(1, LoadSceneMode.Single);
-        Debug.Log("a jugar");
     }
     public void QuitGame()
     {
         Application.Quit();
-        Debug.Log("salir");
+    }
+    public void EndGame()
+    {
+        //SceneManager.LoadScene(2, LoadSceneMode.Single);
+        Time.timeScale = 0.0f;
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
     }
 }
